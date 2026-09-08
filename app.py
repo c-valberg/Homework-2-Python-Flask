425f289c93e4285563adba0d460525f21665061a
Charlie <charlie.vahlberg@gmail.com>
2026-09-08 10:55:25 -0400

    Problem 1

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ from flask import Flask, request
 from markupsafe import escape
 
 app = Flask(__name__)
+
+@app.get('/user/<int:uid>')
+@app.get('/user/@<string:username>')
+@app.get('/user/')
+
+def get_userinfo(uid=None, username=None):
+
+    if uid is not None:
+       return f'User ID: {escape(uid)}'
+
+    elif username is not None:
+       return f'User Profile: {escape(username)}'
+    else:
+       return f'User Profile: guest'
