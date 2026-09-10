29dd7192bc83178fb75ea7cb7f472e73bfa07c49
Charlie <charlie.vahlberg@gmail.com>
2026-09-10 10:10:13 -0400

    I forgot to commit for the last problem

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 @app.get('/user/<int:uid>')
 @app.get('/user/@<string:username>')
 @app.get('/user/')
+@app.get('/lotto/<int:n>/<int:k>')
+@app.get('/search')
 
 def get_userinfo(uid=None, username=None):
 
@@ -18,3 +20,22 @@ def get_userinfo(uid=None, username=None):
        return f'User Profile: {escape(username)}'
     else:
        return f'User Profile: guest'
+
+def get_lotto(n=None, k = None):
+
+   if k>n:
+      return f'Error: sample size k cannot exceed total range', 400
+   
+   lotto_numbers = random.sample(range(1, n + 1), k)
+   return ' '.join(map(str, lotto_numbers))
+   
+def get_search():
+    query = request.args.get('query', 'everything')
+    page = request.args.get('page', '1')
+    
+    try:
+        page = int(page)
+    except ValueError:
+        return 'Error: page must be an integer', 400
+        
+    return f'Page {page} of search results for "{escape(query)}"'
